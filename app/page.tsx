864274a063754493d93a40d7b2e0a36b3e87788a
Ozsumit <oniwagon>
2024-08-18 18:58:50 +0545

    fasdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,60 @@
 import Navigation from "@/components/ui/navigation";
+import { SparklesCore } from "@/components/ui/sparkles";
 import WordFlip from "@/components/ui/textflip";
 // import { WavyBackgroundVer } from "@/components/ui/wave-ver";
 import { WavyBackground } from "@/components/ui/waves";
 import Image from "next/image";
+import { TextGenerateEffect } from "../components/ui/text";
+import { Meteors } from "@/components/ui/meteorcard";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 
 export default function Home() {
   const words = ["Hello", "Namaste", "Ciao", "Konnichiwa", "Bonjour"];
-
+  const wordss = `I haven't done much yet but, here are some of the projects i've done`;
+  const items = [
+    {
+      title: "Repo-Nox",
+      description:
+        "Your friend for elite academic comeback that you dreamed of. Made with NextJS and Tailwind",
+      link: "https://ozsumit.github.io/nox",
+    },
+    {
+      title: "Jyoti EBS ",
+      description:
+        "This is a static website i made for Jyoti English Boarding School. It is made with plain HTML, CSS, & Javascript. ",
+      link: "https://jyotiebs.edu.np/",
+    },
+    {
+      title: "IEMIS",
+      description:
+        "I'm a government certified IEMIS contractor. Not much to say about it.",
+      link: "https://emis.cehrd.gov.np/",
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <WavyBackground>
+    <main className="flex min-h-screen w-[97vw] flex-col items-center justify-between ">
+      <WavyBackground className="z-50">
+        <div className="w-full absolute inset-0 h-screen">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={50}
+            className="w-full h-full  -z-50"
+            particleColor="#FFFFFF"
+          />
+        </div>
         <Navigation />
 
         <div className="maincon flex flex-col justify-center items-center">
           <div
             id="Hero"
-            className="hero flex flex-col justify-center items-center h-screen"
+            className="hero flex z-50 flex-col justify-start mt-[20vh] items-center h-screen"
           >
             <WordFlip
               cursorClassName="text-white blinking-cursor"
-              className="text-7xl flex text-white font-mono font-extrabold"
+              className="text-6xl flex text-white font-mono font-extrabold"
               words={words}
               cursorSymbol="|"
               typingSpeed={200}
@@ -28,7 +63,21 @@ export default function Home() {
               startDelay={300}
             />
 
-            <h1 className="text-7xl text-center sm:w-auto lg:w-3/4    flex text-white font-mono font-extrabold">{`I'm Sumit, a frontend web developer`}</h1>
+            <h1 className="text-6xl text-center sm:w-auto lg:w-3/4    flex text-white font-mono font-extrabold">{`I'm Sumit, a frontend web developer`}</h1>
+          </div>
+        </div>
+
+        {/* </SparklesCore> */}
+        <div className="Work flex flex-col justify-center w-full">
+          <div className="Work flex flex-row justify-between w-full">
+            <div></div>
+            <TextGenerateEffect
+              words={wordss}
+              className="lg:w-5/12  w-auto text-3xl font-mono"
+            />
+          </div>
+          <div className="cards flex justify-center items-center flex-row ">
+            <HoverEffect className="w-10/12" items={items} />
           </div>
         </div>
       </WavyBackground>
---
+++ components/ui/card-hover-effect.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
+// import { useIntersectionObserver } from "@/components/ui/observerapi";
+import styles from "../styles/CardAnimation.module.css";
+import { TextGenerateEffect } from "./text";
+import { Meteors } from "./meteorcard";
+// const cardRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+export const HoverEffect = ({
+  items,
+  className,
+  onCardClick,
+  id,
+}: {
+  items: {
+    title: string;
+    description: string;
+    link: string;
+  }[];
+  className?: string;
+  onCardClick?: (description: string) => void;
+  id?: string;
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [animated, setAnimated] = useState(false);
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const cardRefs = useRef<(HTMLAnchorElement | null)[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !hasAnimated) {
+            setAnimated(true);
+            setHasAnimated(true); // Prevent re-animation on subsequent scrolls
+            observer.disconnect(); // Disconnect observer after animation
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    cardRefs.current.forEach((card) => {
+      if (card) {
+        observer.observe(card);
+      }
+    });
+
+    return () => observer.disconnect();
+  }, [hasAnimated]);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10",
+        className,
+        id
+      )}
+    >
+      {items.map((item, idx) => (
+        <Link
+          href={item.link}
+          key={item.title}
+          className={cn(
+            "relative group block p-2 h-full w-full cursor-pointer ",
+            animated && styles.fadeFromRight
+          )}
+          style={{ animationDelay: `${idx * 200}ms` }}
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          onClick={() => onCardClick && onCardClick(item.description)}
+          ref={(el: HTMLAnchorElement | null) => {
+            if (el) cardRefs.current[idx] = el;
+          }}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-gray-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <div className="rounded-2xl h-56 min-w-54 max-w-120 w-full p-4 overflow-hidden bg-[#000000]/[0.9] border border-white dark:border-white/[0.2] group-hover:border-slate-700 relative z-20">
+            <Meteors />
+            <div className="relative z-48">
+              <div className="p-4">
+                <h4 className="text-zinc-100 font-bold tracking-wide mt-0 text-xl">
+                  {item.title}
+                </h4>
+
+                <p className="mt-5 text-zinc-400 tracking-wide leading-relaxed text-md">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
